6d90a9e371ccf24ad3f97a4ada821de6cdacd30d
Marjiba Jamir <81945731+AdmiralAnne@users.noreply.github.com>
2024-08-01 18:22:40 +0530

    removed some buggy code

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,21 +2,3 @@ import streamlit as st
 import time
 # time library seems to be inbuilt so, no need to intall dependencies
 st.title('⏳ Pomodoro Timer')
-
-def countdown(t): 
-    
-    while t: 
-        mins, secs = divmod(t, 60) 
-        timer = '{:02d}:{:02d}'.format(mins, secs) 
-        print(timer, end="\r") 
-        time.sleep(1) 
-        t -= 1
-      
-    print('Fire in the hole!!') 
-  
-  
-# input time in seconds 
-t = input("Enter the time in seconds: ") 
-  
-# function call 
-countdown(int(t))
