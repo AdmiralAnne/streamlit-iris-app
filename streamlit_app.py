66133eeed009c270ef6f35c8738b42732c860c65
Marjiba Jamir <81945731+AdmiralAnne@users.noreply.github.com>
2024-08-01 18:24:23 +0530

    date time

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import datetime
 import time
 # time library seems to be inbuilt so, no need to intall dependencies
 st.title('⏳ Pomodoro Timer')
+
+t = st.time_input("Select a time", datetime.time(0, 1))
+
+st.write("Time is set to", t)
